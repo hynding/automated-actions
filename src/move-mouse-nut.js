930a7e03ffd2dc00b42a56f6180e5333d722aff3
Steve Hynding <steve.hynding@gmail.com>
2024-12-14 21:14:05 -0800

    os lib alt

---
+++ src/move-mouse-nut.js
@@ -0,0 +1,6 @@
+import { mouse, left, right, up, down } from '@nut-tree-fork/nut-js';
+
+await mouse.move(right(500));
+await mouse.move(down(500));
+await mouse.move(left(500));
+await mouse.move(up(500));
